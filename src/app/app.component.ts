213075c9dcef6fcf3f2676cf22b0ccb021385eee
anton278 <nakonechnyi.an@gmail.com>
2022-07-02 15:00:48 +0300

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RatesService } from './rates.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [RatesService],
 })
-export class AppComponent {
-  title = 'currency-convertor';
+export class AppComponent implements OnInit {
+  constructor(private RatesService: RatesService) {}
+
+  public USDUAH: number = 0;
+  public EURUAH: number = 0;
+
+  setRates() {
+    this.RatesService.getExchangeRates().subscribe((currencies) => {
+      currencies.forEach((currency) => {
+        if (currency.cc === 'USD' && currency.r030 === 840) {
+          this.USDUAH = currency.rate;
+        }
+        if (currency.cc === 'EUR' && currency.r030 === 978) {
+          this.EURUAH = currency.rate;
+        }
+      });
+    });
+  }
+
+  ngOnInit() {
+    this.setRates();
+  }
 }
---
+++ src/app/convertor/convertor.component.ts
@@ -0,0 +1,70 @@
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { calcRate, ICalcRateProps } from 'src/assets/calcRate';
+
+@Component({
+  selector: 'app-convertor',
+  templateUrl: './convertor.component.html',
+  styleUrls: ['./convertor.component.scss'],
+})
+export class ConvertorComponent {
+  @Input() USDUAH!: number;
+  @Input() EURUAH!: number;
+
+  input1 = new FormControl();
+  input2 = new FormControl();
+
+  currency1 = 'UAH';
+  currency2 = 'UAH';
+
+  // calcRateProps: ICalcRateProps = {
+  //   from: this.currency1,
+  //   to: this.currency2,
+  //   count: Number(this.input1.value),
+  //   USDUAH: this.USDUAH,
+  //   EURUAH: this.EURUAH,
+  // };
+
+  select1Handler(e: any) {
+    this.currency1 = e.target.value;
+    const props = {
+      from: this.currency1,
+      to: this.currency2,
+      count: Number(this.input1.value),
+      USDUAH: this.USDUAH,
+      EURUAH: this.EURUAH,
+    };
+    this.input2.setValue(calcRate(props));
+  }
+  select2Handler(e: any) {
+    this.currency2 = e.target.value;
+    const props = {
+      from: this.currency1,
+      to: this.currency2,
+      count: Number(this.input1.value),
+      USDUAH: this.USDUAH,
+      EURUAH: this.EURUAH,
+    };
+    this.input2.setValue(calcRate(props));
+  }
+  input1Handler(e: any) {
+    const props: ICalcRateProps = {
+      from: this.currency1,
+      to: this.currency2,
+      count: Number(e.target.value),
+      USDUAH: this.USDUAH,
+      EURUAH: this.EURUAH,
+    };
+    this.input2.setValue(calcRate(props));
+  }
+  input2Handler(e: any) {
+    const props: ICalcRateProps = {
+      from: this.currency2,
+      to: this.currency1,
+      count: Number(e.target.value),
+      USDUAH: this.USDUAH,
+      EURUAH: this.EURUAH,
+    };
+    this.input1.setValue(calcRate(props));
+  }
+}
